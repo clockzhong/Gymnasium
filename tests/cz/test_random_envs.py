abe5ae424f3391f10fa862459455f72712d45dca
clock zhong <zhong.clock@gmail.com>
2023-10-17 10:07:16 +0800

    some changes

--- tests/cz/test_random_envs.py
+++ tests/cz/test_random_envs.py
@@ -2,23 +2,6 @@
 import gymnasium as gym
 import pytest
 
-envs_list = list(gym.registry.keys())
-
-@pytest.mark.parametrize("env_name", envs_list)
-def test_random_actions(env_name):
-    """random action on one env"""
-    env = gym.make(env_name, render_mode="human")
-    print(f"Executing {env_name}")
-    observation, info = env.reset()
-    for _ in range(10):
-        action = env.action_space.sample()  # agent policy that uses the observation and info
-        observation, reward, terminated, truncated, info = env.step(action)
-
-        if terminated or truncated:
-            observation, info = env.reset()
-
-    env.close()
-
 def random_action_on_env(env_name):
     env = gym.make(env_name, render_mode="human")
     observation, info = env.reset()
@@ -31,10 +14,17 @@ def random_action_on_env(env_name):
 
     env.close()
 
+envs_list = list(gym.registry.keys())
+
+@pytest.mark.parametrize("env_name", envs_list)
+def test_random_actions(env_name):
+    """random action on every env"""
+    random_action_on_env(env_name)
+
 
 
 def test_bug_on_mujoco_lib():
-    """random action on one env"""
+    """Use this case to locate root cause of a special bug relating to mujoco lib"""
     random_action_on_env("Reacher-v2")
     random_action_on_env("Reacher-v5")
     random_action_on_env("Pusher-v2")
