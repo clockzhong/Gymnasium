6da6e0155e66b0f74fa5d7d3e7717fa56d8f3206
clock zhong <zhong.clock@gmail.com>
2026-01-20 16:26:59 +0800

    it seems the mujoco env's bug is fixed, so no need add the workaround logic now, remove them now!

--- tests/cz/test_random_envs.py
+++ tests/cz/test_random_envs.py
@@ -1,17 +1,13 @@
 """run random envs"""
 import gymnasium as gym
 import pytest
+from tests.envs.utils import all_testing_env_specs
 
 def random_action_on_env(env_name, workaround_743=True):
     env = gym.make(env_name, render_mode="human")
-    #entry_point = env.spec.entry_point
+    entry_point = env.spec.entry_point
     #print(env.metadata["render_modes"])
-
-    if workaround_743 and "mujoco" in entry_point:
-    #Workaround for bug: https://github.com/Farama-Foundation/Gymnasium/issues/743
-        env.close()
-        return
-    observation, info = env.reset()
+    _, _ = env.reset()
     for _ in range(10):
         action = env.action_space.sample()  # agent policy that uses the observation and info
         observation, reward, terminated, truncated, info = env.step(action)
@@ -35,21 +31,12 @@ def random_action_on_env_without_reset(env_name):
     return env, observation, reward, terminated, truncated, info
 
 
-envs_list = list(gym.registry.keys())
-envs_list.remove("GymV21Environment-v0")
-envs_list.remove("GymV26Environment-v0")
+#envs_list = list(gym.registry.keys())
+##envs_list.remove("GymV21Environment-v0")
+#envs_list.remove("GymV26Environment-v0")
 
-@pytest.mark.parametrize("env_name", envs_list)
+@pytest.mark.parametrize("env_name", all_testing_env_specs)
 def test_random_actions(env_name):
     """random action on every env"""
     random_action_on_env(env_name)
-
-
-
-def test_bug_on_mujoco_lib():
-    """Use this case to locate root cause of a special bug relating to mujoco lib"""
-    random_action_on_env("Reacher-v2", workaround_743=False)
-    random_action_on_env("Reacher-v5", workaround_743=False)
-    random_action_on_env("Pusher-v2", workaround_743=False)
-
 
