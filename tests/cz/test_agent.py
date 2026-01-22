c9d108af487402bd3d1e334ee67151d233b2e882
clock zhong <zhong.clock@gmail.com>
2026-01-22 21:20:12 +0800

    added test_agent.py into project:cz

--- tests/cz/test_agent.py
+++ tests/cz/test_agent.py
@@ -92,7 +92,7 @@ class BlackjackAgent:
 
 # Training hyperparameters
 learning_rate = 0.01        # How fast to learn (higher = faster but less stable)
-n_episodes = 100_000        # Number of hands to practice
+n_episodes = 1000_000        # Number of hands to practice
 start_epsilon = 1.0         # Start with 100% random actions
 epsilon_decay = start_epsilon / (n_episodes / 2)  # Reduce exploration over time
 final_epsilon = 0.1         # Always keep some exploration
