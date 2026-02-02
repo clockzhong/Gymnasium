148d47feeb8e70fbb7901115568e90976946a05e
clock zhong <zhong.clock@gmail.com>
2026-02-02 11:06:46 +0800

    added get_best_action()

--- tests/cz/test_agent.py
+++ tests/cz/test_agent.py
@@ -1,3 +1,4 @@
+#codes from https://gymnasium.farama.org/introduction/train_agent/ tutorial
 from collections import defaultdict
 import gymnasium as gym
 import numpy as np
@@ -54,6 +55,15 @@ class BlackjackAgent:
         else:
             return int(np.argmax(self.q_values[obs]))
 
+    def get_best_action(self, obs: tuple[int, int, bool]) -> int:
+        """Choose a best action using epsilon-greedy strategy.
+
+        Returns:
+            action: 0 (stand) or 1 (hit)
+        """
+        # With probability epsilon: explore (random action)
+        return int(np.argmax(self.q_values[obs]))
+
     def update(
         self,
         obs: tuple[int, int, bool],
@@ -92,7 +102,7 @@ class BlackjackAgent:
 
 # Training hyperparameters
 learning_rate = 0.01        # How fast to learn (higher = faster but less stable)
-n_episodes = 1000_000        # Number of hands to practice
+n_episodes = 100_000        # Number of hands to practice
 start_epsilon = 1.0         # Start with 100% random actions
 epsilon_decay = start_epsilon / (n_episodes / 2)  # Reduce exploration over time
 final_epsilon = 0.1         # Always keep some exploration
@@ -146,42 +156,69 @@ def get_moving_avgs(arr, window, convolution_mode):
         mode=convolution_mode
     ) / window
 
-# Smooth over a 500-episode window
-rolling_length = 500
-fig, axs = plt.subplots(ncols=3, figsize=(12, 5))
 
-# Episode rewards (win/loss performance)
-axs[0].set_title("Episode rewards")
-reward_moving_average = get_moving_avgs(
-    env.return_queue,
-    rolling_length,
-    "valid"
-)
-axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
-axs[0].set_ylabel("Average Reward")
-axs[0].set_xlabel("Episode")
+def draw():
+    # Smooth over a 500-episode window
+    rolling_length = 500
+    fig, axs = plt.subplots(ncols=3, figsize=(12, 5))
 
-# Episode lengths (how many actions per hand)
-axs[1].set_title("Episode lengths")
-length_moving_average = get_moving_avgs(
-    env.length_queue,
-    rolling_length,
-    "valid"
-)
-axs[1].plot(range(len(length_moving_average)), length_moving_average)
-axs[1].set_ylabel("Average Episode Length")
-axs[1].set_xlabel("Episode")
+    # Episode rewards (win/loss performance)
+    axs[0].set_title("Episode rewards")
+    reward_moving_average = get_moving_avgs(
+        env.return_queue,
+        rolling_length,
+        "valid"
+    )
+    axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
+    axs[0].set_ylabel("Average Reward")
+    axs[0].set_xlabel("Episode")
 
-# Training error (how much we're still learning)
-axs[2].set_title("Training Error")
-training_error_moving_average = get_moving_avgs(
-    agent.training_error,
-    rolling_length,
-    "same"
-)
-axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
-axs[2].set_ylabel("Temporal Difference Error")
-axs[2].set_xlabel("Step")
+    # Episode lengths (how many actions per hand)
+    axs[1].set_title("Episode lengths")
+    length_moving_average = get_moving_avgs(
+        env.length_queue,
+        rolling_length,
+        "valid"
+    )
+    axs[1].plot(range(len(length_moving_average)), length_moving_average)
+    axs[1].set_ylabel("Average Episode Length")
+    axs[1].set_xlabel("Episode")
 
-plt.tight_layout()
-plt.show()
+    # Training error (how much we're still learning)
+    axs[2].set_title("Training Error")
+    training_error_moving_average = get_moving_avgs(
+        agent.training_error,
+        rolling_length,
+        "same"
+    )
+    axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
+    axs[2].set_ylabel("Temporal Difference Error")
+    axs[2].set_xlabel("Step")
+
+    plt.tight_layout()
+    plt.show()
+
+draw()
+
+for episode in tqdm(range(n_episodes)):
+    # Start a new hand
+    obs, info = env.reset()
+    done = False
+
+    # Play one complete hand
+    while not done:
+        # Agent chooses action (initially random, gradually more intelligent)
+        action = agent.get_best_action(obs)
+
+        # Take action and observe result
+        next_obs, reward, terminated, truncated, info = env.step(action)
+
+        # Learn from this experience
+        agent.update(obs, action, reward, terminated, next_obs)
+
+        # Move to next state
+        done = terminated or truncated
+        obs = next_obs
+
+
+draw()
